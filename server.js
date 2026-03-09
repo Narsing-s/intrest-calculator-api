30c715b7640e82c75c81bb49d026cf20bc4224ec
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-03-10 01:51:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,59 +2,47 @@ const express = require("express");
 const fetch = require("node-fetch");
 
 const app = express();
+
 app.use(express.json());
+
+// serve frontend
 app.use(express.static("public"));
 
-const SIMPLE_API = "https://intrest-calculator-api-jik9pb.5sc6y6-4.usa-e2.cloudhub.io/api/simple-interest";
-const COMPOUND_API = "https://intrest-calculator-api-jik9pb.5sc6y6-4.usa-e2.cloudhub.io/api/compound-interest";
+// root route
+app.get("/", (req, res) => {
+  res.send("Interest Calculator API Running 🚀");
+});
+
+const SIMPLE_API =
+  "https://intrest-calculator-api-jik9pb.5sc6y6-4.usa-e2.cloudhub.io/api/simple-interest";
+
+const COMPOUND_API =
+  "https://intrest-calculator-api-jik9pb.5sc6y6-4.usa-e2.cloudhub.io/api/compound-interest";
 
 app.post("/simple-interest", async (req, res) => {
+  const response = await fetch(SIMPLE_API, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(req.body),
+  });
 
-    try {
-
-        const response = await fetch(SIMPLE_API,{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(req.body)
-        });
-
-        const data = await response.json();
-
-        res.json(data);
-
-    } catch(error){
-        res.status(500).json({error:"API call failed"});
-    }
-
+  const data = await response.json();
+  res.json(data);
 });
-
 
 app.post("/compound-interest", async (req, res) => {
+  const response = await fetch(COMPOUND_API, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(req.body),
+  });
 
-    try {
-
-        const response = await fetch(COMPOUND_API,{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(req.body)
-        });
-
-        const data = await response.json();
-
-        res.json(data);
-
-    } catch(error){
-        res.status(500).json({error:"API call failed"});
-    }
-
+  const data = await response.json();
+  res.json(data);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
